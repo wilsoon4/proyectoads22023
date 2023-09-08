bda73b555e8f127799b733447ef91503b32553b9
David Carrillo <carrillod421@gmail.com>
2023-09-08 07:59:36 -0600

    Integrante1 David Carrillo

--- Codigo/Pruebas/Navegador/Prototipo_inicial_nav/Prototipo_inicial_nav/frmIntegrante1.Designer.cs
+++ Codigo/Pruebas/Navegador/Prototipo_inicial_nav/Prototipo_inicial_nav/frmIntegrante1.Designer.cs
@@ -47,7 +47,7 @@ namespace Prototipo_inicial_nav
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(33, 141);
+            this.label2.Location = new System.Drawing.Point(21, 98);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(159, 29);
             this.label2.TabIndex = 1;
